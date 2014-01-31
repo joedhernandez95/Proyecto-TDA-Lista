916551f93a6427067af259ccb8837b7246cf038b
Jose Hernandez <joedhernandez95@hotmail.com>
2014-01-31 16:11:17 -0600

    Agregando Clase Nodo para Lista Enlazada Sencilla

---
+++ slnode.cpp
@@ -0,0 +1,45 @@
+#include "slnode.h"
+#include "object.h"
+#include <iostream>
+using namespace std;
+
+SLNode::SLNode(){
+	next = NULL;
+	data = NULL;
+}
+
+SLNode::SLNode(OBject* aData){
+	data = aData;
+}
+
+SLNode::SLNode(OBject* aData,SLNode* aNext){
+	data = aData;
+	next = aNext;
+}
+
+SLNode::~SLNode(){
+	if(data)
+		delete data;
+	if(!(next==NULL))
+		delete next;
+}
+
+void SLNode::print()const{
+	data->print();
+}
+
+SLNode* SLNode::getNext()const{
+	return next;
+}
+
+void SLNode::setNext(SLNode* aNext){
+	next = aNext;
+}
+
+Object* SLNode::getData()const{
+	return data;
+}
+
+void SLNode::setData(Object* aData){
+	data = aData;
+}
