eac4d2e9c6717a4c12c1cac077eac2c287b78ba3
Jose Hernandez <joedhernandez95@hotmail.com>
2014-01-31 16:37:46 -0600

    Se modifico SLINKEDLIST.H

---
+++ slinkedlist.h
@@ -0,0 +1,25 @@
+#ifndef SLINKEDLIST_H
+#define SLINKEDLIST_H
+#include "object.h"
+#include "slnode.h"
+#include <tdalist.h>
+
+class SLinkedList : public TDAList{
+protected:
+	SLNode* head;
+public:
+	SLinkedList();
+	virtual ~SLinkedList();
+	virtual bool insert(Object*,int);
+	virtual int indexOf(OBject*)const;
+	virtual Object* get(unsigned)const;
+	virtual bool erase(unsigned);
+	virtual int prev(int)const;
+	virtual int next(int)const;
+	virtual void reset();
+	virtual Object* first()const;
+	virtual Object* last()const;
+	virtual void print()const;
+	virtual bool isEmpty()const;
+	virtual bool isFull()const;
+};
